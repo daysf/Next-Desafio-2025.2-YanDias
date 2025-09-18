208ae3f35749a77b5ff54431d9ba31f23619fa83
Yan Dias <yandiasferreira@gmail.com>
2025-09-17 23:22:23 -0300

    segue um pedaço do tutorial

---
+++ components/search/index.tsx
@@ -0,0 +1,12 @@
+import { Search as SearchIcon } from "lucide-react";
+
+export default function Search(){
+    return(
+        <form className="flex basis-full" autoComplete="off">
+            <div className="flex w-full flwx-col items-center gap-6 my-10">
+                <div className="relative flex w-full items-center">
+                </div>
+            </div>
+        </form>
+    )
+}
