76771063bd704d524d201c28c731b8d3cca1e1ee
Yan Dias <yandiasferreira@gmail.com>
2025-09-19 02:19:20 -0300

    tenta implementar toggleNavbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,18 +1,23 @@
 'use client'
 
-import { ContactRound } from "lucide-react"
+import { ContactRound, Menu, X } from "lucide-react"
 import { useState} from "react"
 import Image from "next/image";
 import Link from "next/link";
+import Search from "../search";
 
 
 const links = [
+    { href: '/posts', label: 'Home'},
     { href: '/posts', label: 'Contato'},
     { href: '/posts', label: 'Produtos'},    
     { href: '/admin', label: 'Gerenciamento'},
     { href: '/posts', label: 'Carrinho'},
+]
+
+const links2 = [
     { href: '/posts', label: 'Registro'},
-    { href: '/posts', label: 'Cadastro'}
+    { href: '/posts', label: 'Login'},
 ]
 
 export default function Navbar(){
@@ -20,24 +25,48 @@ export default function Navbar(){
     const toggleNavbar = () => setIsNavbarOpen (!isNavbarOpen)
 
     return(
-        <div className="bg-gradient-to-r from-verde4 to-verde2 sticky mx-auto w-full flex py-8 px-4 md:p-2 mb-4">
-            <Image src={'/Logo.png'}
+        <header className="flex bg-gradient-to-r from-verde4 to-verde2 sticky mx-auto w-full py-8 px-4 md:p-0 mb-8">
+                <Image src={'/Logo.png'}
                 alt="Logo da loja"
                 width={904}
                 height={904}
-                className="h-20 w-20 rounded-x1">
-            </Image>
-            <div href="/" className="text-offwhite font-medium flex w-full gap-5 justify-center items-center">
-                <span className="hover:bg-rosa4 px-3 py-1 rounded">Home</span>
-                <span className="hover:bg-rosa4 px-3 py-1 rounded">Contato</span> 
-                <span className="hover:bg-rosa4 px-3 py-1 rounded">Gerenciamento</span>
-                <span className="hover:bg-rosa4 px-3 py-1 rounded">Produtos</span>
-                <span className="hover:bg-rosa4 px-3 py-1 rounded">Carrinho</span>
-            </div>
-            <div href="/" className="text-offwhite font-bold flex gap-5 justify-end items-center px-4">
-                <span className="border border-rosa4 rounded-sm px-6 py-1">Login</span>
-                <span className="bg-gradient-to-bl from-rosa4 to-rosa3 border border-rosa4 rounded-sm px-4 py-1">Register</span>
-            </div>
-        </div>     
+                className="h-20 w-20 rounded-x1 bp:ml-5">
+                </Image>
+                <nav className="text-offwhite font-medium flex w-full justify-center items-center lg:gap-5 xl:gap-15">
+                    {links.map((link, index) =>
+                        <Link href={link.href} key={index}>
+                            <span className="hover:bg-rosa4 rounded md:px-3 py-1">{link.label}</span>
+                        </Link>
+                    )}
+                </nav>
+                <nav href="/" className="text-offwhite font-bold flex gap-5 justify-end items-center px-4">
+                    <span className="border border-rosa4 rounded-sm px-6 py-1">Login</span>
+                    <span className="bg-gradient-to-bl from-rosa4 to-rosa3 border border-rosa4 rounded-sm px-4 py-1">Register</span>
+                </nav>
+                <div className="md:hidden">
+                    {isNavbarOpen ?
+                        <X
+                        onClick={toggleNavbar}
+                        className="w-10 h-10 text-offwhite cursor-pointer"
+                        />
+                        :
+                        <div className="flex gap-4">
+                            <Menu
+                            onClick={toggleNavbar}
+                            className="w-12 h-12 text-offwhite cursor-pointer"
+                            />
+                        </div>
+                    }
+                </div>
+                {isNavbarOpen && (
+                    <div className="md:hidden flex basis-full flex-col items-center gap-6 mt-10">
+                        {links.map((link, index) =>
+                            <Link href={link.href} key={index}>
+                                <span className="hover:bg-rosa4 rounded md:px-3 py-1">{link.label}</span>
+                            </Link>
+                        )}
+                    </div>
+                )}
+        </header>     
     )
 }
