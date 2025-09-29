02ada05a240a274df36ffc415a7af8bf7a769cce
Yan Dias <yandiasferreira@gmail.com>
2025-09-29 03:58:20 -0300

    modifica pasta do sobre nós

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 import HeroSection from "@/components/heroSection";
-import MVV from "@/components/heroSection/mvv";
+import MVV from "@/components/mvv";
 import Login from "@/components/login";
 import Registro from "@/components/registro";
 
